5c2131d1518e6c22ccad45b1db4bb3aae2a59409
Zino Hofmann <zino@hofmann.amsterdam>
2017-05-05 00:01:29 +0200

    Testing sreach placeholder alligment

--- src/Toolbar/CenterElement.react.js
+++ src/Toolbar/CenterElement.react.js
@@ -96,7 +96,7 @@ class CenterElement extends PureComponent {
                     onChangeText={onSearchTextChange}
                     onSubmitEditing={searchable.onSubmitEditing}
                     placeholder={searchable.placeholder}
-                    style={[styles.titleText, { marginLeft: 0 }]}
+                    style={[styles.titleText, { marginLeft: 0, textAlign: 'center' }]}
                     underlineColorAndroid="transparent"
                     value={searchValue}
                 />
